feb4ba1ccf48a2fc93df3c402120d62eed855979
Matthias Endler <matthias-endler@gmx.net>
2018-03-09 16:41:50 +0100

    Add open and logs commands. Improve error handling

--- main.go
+++ main.go
@@ -10,18 +10,29 @@ import (
 
 	"github.com/bndr/gojenkins"
 	"github.com/fatih/color"
+	"github.com/skratchdot/open-golang/open"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	// TODO: Replace this with a custom formatter or so
 	statusCommand  = kingpin.Command("status", "Show the status of all matching jobs")
 	statusRegexArg = statusCommand.Arg("regex", "The regular expression to match for the job names").Default(".*").String()
-	verbose        = statusCommand.Flag("verbose", "Verbose mode. Print full job output").Short('v').Bool()
-	salt           = statusCommand.Flag("salt", "Show failed salt states").Bool()
 
-	queueCommand = kingpin.Command("queue", "Show the queue of all matching jobs")
+	logsCommand = kingpin.Command("logs", "Show the logs of a job")
+	logsJobArg  = logsCommand.Arg("job", "The name of the job to get logs for").String()
+
+	queueCommand  = kingpin.Command("queue", "Show the queue of all matching jobs")
+	queueRegexArg = queueCommand.Arg("regex", "The regular expression to match for the job names").Default(".*").String()
+
 	nodesCommand = kingpin.Command("nodes", "Show the status of all Jenkins nodes")
+
+	openCommand  = kingpin.Command("open", "Open a job in the browser")
+	openRegexArg = openCommand.Arg("regex", "The regular expression to match for the job names").Default(".*").String()
+
+	verbose = kingpin.Flag("verbose", "Verbose mode. Print full job output").Short('v').Bool()
+
+	// TODO: Replace this with a custom formatter or so
+	salt = kingpin.Flag("salt", "Show failed salt states").Bool()
 )
 
 func getFailedSaltStates(output string) []string {
@@ -35,10 +46,9 @@ func getFailedSaltStates(output string) []string {
 	return failedStates
 }
 
-func printStatus(waitGroup *sync.WaitGroup, jenkins *gojenkins.Jenkins, job gojenkins.InnerJob, verbose, salt bool) error {
-	// Buffer full output to avoid race conditions between jobs
-	output := ""
+func printStatus(waitGroup *sync.WaitGroup, jenkins *gojenkins.Jenkins, job gojenkins.InnerJob) error {
 	defer waitGroup.Done()
+	// Buffer full output to avoid race conditions between jobs
 	yellow := color.New(color.FgYellow).SprintFunc()
 	red := color.New(color.FgRed).SprintFunc()
 	green := color.New(color.FgGreen).SprintFunc()
@@ -66,23 +76,50 @@ func printStatus(waitGroup *sync.WaitGroup, jenkins *gojenkins.Jenkins, job goje
 		marker = yellow("?")
 	}
 
-	output += fmt.Sprintf("%v %v (%v)\n", marker, job.Name, job.Url)
+	fmt.Printf("%v %v (%v)\n", marker, job.Name, job.Url)
+	return nil
+}
 
-	if result != "SUCCESS" && lastBuild != nil {
-		if verbose || salt {
-			output += fmt.Sprintf("Jenkins result code: %v\n", result)
-			consoleOutput := lastBuild.GetConsoleOutput()
-			if salt {
-				output += fmt.Sprintf(consoleOutput)
-			} else {
-				for _, stateOutput := range getFailedSaltStates(consoleOutput) {
-					output += stateOutput
-				}
-			}
-			output += fmt.Sprintf("%v/consoleText\n", lastBuild.GetUrl())
-		}
+func logsExec(jenkins *gojenkins.Jenkins, jobName string, salt bool) error {
+	yellow := color.New(color.FgYellow).SprintFunc()
+	red := color.New(color.FgRed).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+
+	build, err := jenkins.GetJob(jobName)
+	if err != nil {
+		return err
 	}
-	fmt.Print(output)
+
+	lastBuild, err := build.GetLastBuild()
+	var result string
+	if err != nil {
+		result = fmt.Sprintf("UNKNOWN (%v)", err)
+	} else {
+		result = lastBuild.GetResult()
+	}
+
+	var marker string
+	switch result {
+	case "SUCCESS":
+		marker = green("✓")
+	case "FAILURE":
+		marker = red("✗")
+	default:
+		marker = yellow("?")
+	}
+
+	fmt.Printf("%v %v (%v)\n", marker, jobName, lastBuild.GetUrl())
+
+	fmt.Printf("Jenkins result code: %v\n", result)
+	consoleOutput := lastBuild.GetConsoleOutput()
+	if salt {
+		for _, stateOutput := range getFailedSaltStates(consoleOutput) {
+			fmt.Println(stateOutput)
+		}
+	} else {
+		fmt.Printf(consoleOutput)
+	}
+	fmt.Printf("%v/consoleText\n", lastBuild.GetUrl())
 	return nil
 }
 
@@ -104,15 +141,16 @@ func findMatchingJobs(jenkins *gojenkins.Jenkins, regex string) ([]gojenkins.Inn
 	return matchingJobs, nil
 }
 
-func queue(jenkins *gojenkins.Jenkins, regex string, verbose, salt bool) {
+func queue(jenkins *gojenkins.Jenkins, regex string, verbose, salt bool) error {
 	queue, err := jenkins.GetQueue()
 	if err != nil {
-		log.Fatalf("%v\n", err)
+		return err
 	}
 	fmt.Println(queue.Raw)
 	// for _, task := range tasks {
 	// 	fmt.Println(task.GetWhy())
 	// }
+	return nil
 
 }
 
@@ -132,10 +170,10 @@ func printNodeStatus(waitGroup *sync.WaitGroup, node gojenkins.Node) error {
 	return nil
 }
 
-func nodes(jenkins *gojenkins.Jenkins, regex string, verbose, salt bool) {
+func nodes(jenkins *gojenkins.Jenkins) error {
 	nodes, err := jenkins.GetAllNodes()
 	if err != nil {
-		log.Fatalf("%v\n", err)
+		return err
 	}
 
 	var waitGroup sync.WaitGroup
@@ -144,20 +182,37 @@ func nodes(jenkins *gojenkins.Jenkins, regex string, verbose, salt bool) {
 	for _, node := range nodes {
 		go printNodeStatus(&waitGroup, *node)
 	}
+	return nil
 }
 
-func status(jenkins *gojenkins.Jenkins, regex string, verbose, salt bool) {
+func openExec(jenkins *gojenkins.Jenkins, regex string) error {
 	jobs, err := findMatchingJobs(jenkins, regex)
 	if err != nil {
-		panic(fmt.Sprintf("Cannot retrieve jobs: %v", err))
+		return err
+	}
+	if len(jobs) > 3 {
+		log.Fatalf("More than three jobs match your criteria. This is probably not what you expected. Please narrow down your search\n")
+	}
+
+	for _, job := range jobs {
+		open.Run(job.Url)
+	}
+	return nil
+}
+
+func statusExec(jenkins *gojenkins.Jenkins, regex string) error {
+	jobs, err := findMatchingJobs(jenkins, regex)
+	if err != nil {
+		return err
 	}
 
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(len(jobs))
 	defer waitGroup.Wait()
 	for _, job := range jobs {
-		go printStatus(&waitGroup, jenkins, job, verbose, salt)
+		go printStatus(&waitGroup, jenkins, job)
 	}
+	return nil
 }
 
 func main() {
@@ -182,12 +237,20 @@ func main() {
 	// TODO: Replace with a plugin-based system
 	switch kingpin.Parse() {
 	case "status":
-		status(jenkins, *statusRegexArg, *verbose, *salt)
+		err = statusExec(jenkins, *statusRegexArg)
+	case "logs":
+		err = logsExec(jenkins, *logsJobArg, *salt)
 	case "queue":
-		queue(jenkins, *statusRegexArg, *verbose, *salt)
+		err = queue(jenkins, *queueRegexArg, *verbose, *salt)
 	case "nodes":
-		nodes(jenkins, *statusRegexArg, *verbose, *salt)
+		err = nodes(jenkins)
+	case "open":
+		err = openExec(jenkins, *openRegexArg)
 	default:
 		kingpin.Usage()
+	}
+
+	if err != nil {
+		log.Fatalf("Cannot execute command: %v", err)
 	}
 }
